79a0f780bd762a26b57b82e5011be183b7bc3c8a
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2024-01-21 21:03:11 -0500

    Feature: Private Routing and same page

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,12 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRouter } from "./PublicRoutes";
 export const AppRouter = () => {
   return (
     <>
       <div className="container">
         <Routes>
-          <Route path="login" element={<LoginPage />} />
+
+          <Route path="/login" element={
+            <PublicRouter>
+              <LoginPage />
+            </PublicRouter>
+          }>
+
+          </Route>
+          {/* <Route path="login" element={<LoginPage />} /> */}
 
           <Route path="/*" element={
             <PrivateRoute>  
