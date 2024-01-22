d6663d5fd88916ed39bc8506f46d7ff905e25364
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2024-01-21 20:50:53 -0500

    Feature: Rutes privates

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,13 +2,20 @@ import { Route, Routes } from "react-router-dom";
 
 import { LoginPage } from "../auth/pages/LoginPage";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 export const AppRouter = () => {
   return (
     <>
       <div className="container">
         <Routes>
           <Route path="login" element={<LoginPage />} />
-          <Route path="/*" element={<HeroesRoutes />} />
+
+          <Route path="/*" element={
+            <PrivateRoute>  
+              <HeroesRoutes />
+            </PrivateRoute>
+          }></Route>
+          {/* <Route path="/*" element={<HeroesRoutes />} /> */}
         </Routes>
       </div>
     </>
